f1f9c9ce9e6ade032f9ea150aec4c5ba5ec468be
Erno Kaikkonen <erno@ampli.fi>
2013-10-12 22:07:34 +0300

    first version

---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+// This is a test script demonstrating the use of the gd-heatmap library.
+require_once('gd_heatmap.php');
+
+// Generate some test data using the static function provided for that purpose.
+$data = gd_heatmap::get_test_data(1240, 600, 50, 500);
+
+// Config array with all the available options. See the constructor's doc block
+// for explanations.
+$config = array(
+  'debug' => TRUE,
+  'width' => 1240,
+  'height' => 600,
+  'noc' => 32,
+  'r' => 50,
+  'dither' => FALSE,
+  'format' => 'jpeg',
+);
+
+// Create a new heatmap based on the data and the config.
+$heatmap = new gd_heatmap($data, $config);
+
+// And print it out. If you're having trouble getting any images out of the
+// library , comment this out to allow your browser to show you the error
+// messages.
+$heatmap->output();
